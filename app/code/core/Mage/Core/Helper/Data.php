135da319087d40c6282c88feb8027ba115abde91
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-03-10 13:14:20 +0100

    changed for better readability

--- app/code/core/Mage/Core/Helper/Data.php
+++ app/code/core/Mage/Core/Helper/Data.php
@@ -318,9 +318,10 @@ class Mage_Core_Helper_Data extends Mage_Core_Helper_Abstract
 
             if ($german) {
                 // umlauts
-                $subst = array(
+                $germanReplacements = array(
                     196 => 'Ae', 228 => 'ae', 214 => 'Oe', 246 => 'oe', 220 => 'Ue', 252 => 'ue'
-                ) + $subst;
+                );
+                $subst = $germanReplacements + $subst;
             }
 
             $replacements[$german] = array();
