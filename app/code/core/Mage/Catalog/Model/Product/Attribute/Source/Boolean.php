db22fde5d7a203cbea7545a108a6ee216ccab771
Vinai Kopp <vinai@netzarbeiter.com>
2013-03-11 17:33:24 +0100

    Implement the addValueSortToCollection logic for the flat catalog

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Source/Boolean.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Source/Boolean.php
@@ -58,4 +58,24 @@ class Mage_Catalog_Model_Product_Attribute_Source_Boolean extends Mage_Eav_Model
         }
         return $this->_options;
     }
+
+    /**
+     * Add Value Sort To Collection Select
+     *
+     * @param Mage_Catalog_Model_Resource_Product_Collection $collection
+     * @param string $dir direction
+     * @return Mage_Catalog_Model_Product_Attribute_Source_Abstract
+     */
+    public function addValueSortToCollection($collection, $dir = Varien_Data_Collection::SORT_ORDER_DESC)
+    {
+        if (method_exists($collection, 'isEnabledFlat') && $collection->isEnabledFlat()) {
+            $collection->addAttributeToSelect($this->getAttribute()->getAttributeCode())
+                ->getSelect()
+                    ->order("{$this->getAttribute()->getAttributeCode()} {$dir}");
+        } else {
+            return parent::addValueSortToCollection($collection, $dir);
+        }
+        return $this;
+    }
+
 }
